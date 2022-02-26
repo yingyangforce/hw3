1dd06d4b1cb1bb40c9434a5268c90067024d641c
Gene Calmer <yingyangforce@gmail.com>
2022-02-25 23:03:18 -0500

    final changes

--- grace_hw3b.py
+++ grace_hw3b.py
@@ -4,8 +4,6 @@
 
 #---Part B---
 
-# TODO: add input and prompts
-
 def CelsiusToFahrenheit(temp):
     # F = C * (9/5) + 32
     return temp * (9/5) + 32
@@ -29,4 +27,54 @@ def KelvinToCelsius(temp):
 def KelvinToFahrenheit(temp):
     # F = (K - 273.15) * (9/5) + 32
     return (temp - 273.15) * (9/5) + 32
+
+# ask for input measure (F/C/K), output measure, temperature reading
+# output new temp
+# TODO: follow up on KFC sponsorship
+
+input_measure = input("What measure does your temp use? ['K'/'F'/'C']: ")
+
+while input_measure not in ('K', 'F', 'C'):
+    print("ERR: Please enter 'K', 'F', or 'C'.")
+    input_measure = input("What measure does your temp use? ['K'/'F'/'C']: ")
+
+#---
+
+output_measure = input("What measure do you want to convert to? ['K'/'F'/'C']: ")
+
+while output_measure not in ('K', 'F', 'C'):
+    print("ERR: Please enter 'K', 'F', or 'C'.")
+    output_measure = input("What measure do you want to convert to? ['K'/'F'/'C']: ")
+
+#---
+
+while True:
+    try:
+        input_temp = float(input("What temp do you want to convert? (Decimal): "))
+    except:
+        print("ERR: Please enter temp in decimal format.")        
+        continue
+    break
+
+if (input_measure, output_measure) == ('K', 'F'):
+    print(f"Your new temp is {KelvinToFahrenheit(input_temp)}.")
+
+if (input_measure, output_measure) == ('K', 'C'):
+    print(f"Your new temp is {KelvinToCelsius(input_temp)}.")
+
+if (input_measure, output_measure) == ('F', 'K'):
+    print(f"Your new temp is {FahrenheitToKelvin(input_temp)}.")
+
+if (input_measure, output_measure) == ('F', 'C'):
+    print(f"Your new temp is {FahrenheitToCelsius(input_temp)}.")
+
+if (input_measure, output_measure) == ('C', 'K'):
+    print(f"Your new temp is {CelsiusToKelvin(input_temp)}.")
+
+if (input_measure, output_measure) == ('C', 'F'):
+    print(f"Your new temp is {CelsiusToFahrenheit(input_temp)}.")
+
+if input_measure == output_measure:
+    print("Your temp is still", input_temp, end=".\n")
+
 
